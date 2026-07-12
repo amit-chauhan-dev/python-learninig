7e4c138233f838874a69dc7b2d16c57fd8c2cd3b
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-12 10:41:18 +0530

    Add 03_type_casting.py

---
+++ 03_type_casting.py
@@ -0,0 +1,12 @@
+#  type casting
+age = 25
+age_str = str(age)
+print("my age is " + age_str)
+
+num = "100"
+num_int = int(num)
+print(num_int + 50)
+
+height = 5.4
+height_int = int(height)   # float → int (removes decimal)
+print(height_int)
