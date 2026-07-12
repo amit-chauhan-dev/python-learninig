051ad7af7c4351d8d133d8b1f55b89d9f5c3da33
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-12 11:24:03 +0530

    the are operators

---
+++ 05_operators.py
@@ -0,0 +1,41 @@
+# arithmetic operators
+
+a = 10 
+b = 3
+
+print(a + b)    # 13
+print(a - b)    # 7
+print(a * b)    # 30
+print(a / b)    # 3.333333333
+print(a // b)   # 3 (floor division)
+print(a % b)    # 1 ( remainder)
+print(a ** b)   # 1000 (exponent)
+
+
+# Comparison Operators (return True/False)
+
+print(10 == 10) # True
+print(10 != 5)  # True
+print(10 > 5)   # True
+print(10 <= 10) # True
+
+
+# logical operators
+
+x = True
+y = False
+
+print(x and y)  # False
+print(x or y)   # True 
+print(not x)    # False
+
+
+# assignment operators
+
+x = 10 
+x += 5  # x = x + 5 -> 15
+print(x)
+x -= 3  # x = x - 3 -> 12
+print(x)
+x *= 2  # x = x * 2 -> 24
+print(x)
