b2698b75ec81db36d7066ad0fda08c7c4c3b2d28
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-14 11:24:07 +0530

    Nested condition (if inside if)

---
+++ 07_nested_condition.py
@@ -0,0 +1,14 @@
+username = "admin"
+password = "1234"
+
+if username == "admin":
+    if password == "1234":
+        print("Logine successful ")
+    else:
+        print("Wrong password ")
+else:
+    print("user not found")
+
+
+# Tip:
+'try to avoid too many nested levels(more than 3) - it becomes hard to read.'
