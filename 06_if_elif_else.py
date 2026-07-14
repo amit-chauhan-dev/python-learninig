8f42f98d9c443e85a1084cb0d7d5333bc1053cdc
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-14 11:16:21 +0530

    Basic if, elif, else

---
+++ 06_if_elif_else.py
@@ -0,0 +1,10 @@
+# if, elif, else (Dicision making)
+
+# bacic if 
+age  = 20
+if age >= 18:
+    print("you are an adult ")
+elif age >= 13:
+    print("you are a teenager ")
+else:
+    print("you are a child ")
