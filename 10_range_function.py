830325e0b32241265cb50be26b1847b8886c1de3
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-14 11:50:13 +0530

    range() function is Very important

---
+++ 10_range_function.py
@@ -0,0 +1,11 @@
+# range() function (very important)
+'range (start, stop, step)'
+
+for i in range(1, 11):  # 1 to 10
+    print(i)
+
+for i in range(0, 10, 2):   # even numbers
+    print(i)
+
+for i in range(10, 0, -1):  # countdown
+    print(i)
