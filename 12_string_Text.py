a209b33059bb873514ae3b9118cbe9de9c94a609
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-27 10:47:01 +0530

    String (Text)

---
+++ 12_string_Text.py
@@ -0,0 +1,21 @@
+# Strings (Text)
+
+name = "Python Programing"
+
+# Slicing
+print(name[0])  # P
+print(name[0:6])    # Python
+print(name[-1])     # g (last character)
+print(name[::-1])   # reverse string 
+
+# Common Method
+print(name.upper())
+print(name.lower())
+print(name.replace("Python", "Java"))
+print(name.split())
+print(len(name))
+
+text = "I love learning Python"
+# Make it all uppercase and revese it 
+print(text.upper())
+print(text[::-1])
