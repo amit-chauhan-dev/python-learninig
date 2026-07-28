64371a7f0ef36227b65d888e9c8d7d4273f05e21
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-28 10:15:54 +0530

    Lists are mutable (you can change them )

--- 14_tuples_immutable_sequences.py
+++
@@ -1,8 +0,0 @@
-# tuples (immutable lists)
-
-coordinates = (10, 20)
-colors = ("red", "green", "blue")
-
-print(coordinates[0])
-# coordinates[0] = 50 <-- this will give ERROR (immutable)
-# useful when data should never change
--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-# # method 1: Direct
-# for num in numbers:
-#     print(num)
-
-# # method 2: with Index
-# for i, num in enumerate (numbers):
-#     print(num)
