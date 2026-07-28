4d6f0b3dfb649bba1e10097e9c574290fae0c9ec
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-28 10:21:32 +0530

    tuples (immutable)

---
+++ 14_tuples_immutable_sequences.py
@@ -0,0 +1,8 @@
+# tuples (immutable lists)
+
+coordinates = (10, 20)
+colors = ("red", "green", "blue")
+
+print(coordinates[0])
+# coordinates[0] = 50 <-- this will give ERROR (immutable)
+# useful when data should never change
