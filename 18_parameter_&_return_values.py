6f420204a1084452ed337037336b873eb43a97ff
Amit Chauhan <amitchauhan18590@gmail.com>
2026-07-28 11:34:04 +0530

    parameter & return values

---
+++ 18_parameter_&_return_values.py
@@ -0,0 +1,14 @@
+#  parameter & return values
+
+def add(a, b):      # parameter
+    result = a + b
+    return result   # return value
+
+sum1 = add(10, 20)
+print(sum1)     # 30
+
+print(add(5, 15))   # can also print directly
+
+# key point.
+# parameter  -> input to the function 
+# return -> output from the function
