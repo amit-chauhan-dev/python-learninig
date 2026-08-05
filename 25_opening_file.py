b618c1fa07d587db4d87594328301cbb40b32c03
Amit Chauhan <amitchauhan18590@gmail.com>
2026-08-05 10:53:00 +0530

    opening a file - open()

---
+++ 25_opening_file.py
@@ -0,0 +1,12 @@
+# opening a file - open()
+
+file = open("filename.txt", "mode")
+
+# Common Mode
+# Mode,   Meaning,          Description
+#  r,     Read (default),   Opens file for reading
+#  w,     Write,            Creates new file or overwrites existing file
+#  a,     Append,           Adds data at the end of the file
+#  x,     Create,           "Creates file, error if already exists"
+#  rb,    Read Binary,      "For images, PDFs, etc."
+#  wb,    Write Binary,     For writing binary data
