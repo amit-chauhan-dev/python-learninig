c55de812475f6267f4b28953a053e7f82f8d2aec
Amit Chauhan <amitchauhan18590@gmail.com>
2026-08-07 13:09:59 +0530

    Class and Object

---
+++ 37_class_and_object.py
@@ -0,0 +1,13 @@
+# Class and Object:
+
+# class = Blueprint / template
+# object = Actual thing created from the class
+
+class Student:
+    pass
+
+# Creating objects
+s1 = Student()
+s1 = Student()
+
+print(type(s1))     # <class '__main__.student>
